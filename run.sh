a84b2710e4556cc57f8edc99c596e206eb84a35c
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2026-06-24 18:19:03 +0200

    dynamically choose target framework depending on os kernel

--- run.sh
+++ run.sh
@@ -7,5 +7,10 @@ set -euo pipefail
 SolutionDir="$(realpath "$(dirname "${BASH_SOURCE[0]}")")/"
 cd "$SolutionDir"
 
+tf="net8.0"
+case "$(uname -s | tr '[:upper:]' '[:lower:]')" in
+  mingw*|msys*|cygwin*|windows*) tf="net8.0-windows" ;;
+esac
+
 bash ./build.sh
-SolutionDir="$SolutionDir" bash -c 'dotnet run "./LD44.sln" --project "./LD44/" --framework net8.0-windows --configuration Debug'
+SolutionDir="$SolutionDir" bash -c 'dotnet run "./LD44.sln" --project "./LD44/" --framework "'"$tf"'" --configuration Debug'
